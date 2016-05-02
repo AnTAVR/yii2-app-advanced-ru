764182f78870fcac89604486724d4874b922d90b
Anthony Lyappiev <admin@antavr.ru>
2016-05-02 20:35:59 +0300

    перенес общий AppAsset

--- common/assets/AppAsset.php
+++ common/assets/AppAsset.php
@@ -1,16 +1,15 @@
 <?php
 
-namespace frontend\assets;
+namespace common\assets;
 
 use yii\web\AssetBundle;
 
 /**
- * Main frontend application asset bundle.
+ * All web-application asset bundle.
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $sourcePath = '@common/assets/assets';
     public $css = [
         'css/site.css',
     ];
