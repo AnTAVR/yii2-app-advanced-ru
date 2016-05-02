2c833f91a23e25a8e4ba34ec959ed30f9a5219be
Anthony Lyappiev <admin@antavr.ru>
2016-05-02 19:32:53 +0300

    добавил 'brandLabel в параметры

--- tests/codeception/frontend/functional/HomeCept.php
+++ tests/codeception/frontend/functional/HomeCept.php
@@ -10,7 +10,7 @@ use tests\codeception\frontend\FunctionalTester;
 $I = new FunctionalTester($scenario);
 $I->wantTo(Yii::t('test', 'ensure that home page works'));
 $I->amOnPage(Yii::$app->homeUrl);
-$I->see('My Company');
+$I->see(Yii::$app->params['brandLabel']);
 $I->seeLink(Yii::t('app', 'About'));
 $I->click(Yii::t('app', 'About'));
 $I->see(Yii::t('app', 'About'), 'h1');
